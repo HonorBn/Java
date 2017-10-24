ca0b7a8efb88c5169dffcff011a977632adc3ba4
youngbin-ahn <ahnbnn@gmail.com>
2017-10-24 19:53:07 +0900

    test

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		input();
+		
+		process();
+		
+		print();
+		
+	}
+
+	private static void input() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+	
+	private static int process() {
+		return 0;
+	}
+	
+	private static void print() {
+		
+	}
+	
+}
