4156e6cc6112cc5110abf55d0145178aeb1ce8f5
youngbin-ahn <ahnbnn@gmail.com>
2017-10-24 21:47:51 +0900

    Source Code Form

---
+++ 00. Form/Form_Normal.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Form_Normal {
+	
+	public static void main(String[] args) throws IOException {
+		
+		input();
+		
+		process();
+		
+		print();
+		
+	}
+
+	private static void input() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.close();
+		
+	}
+	
+	private static int process() {
+		
+		return 0;
+	}
+	
+	private static void print() {
+		
+	}
+	
+}
