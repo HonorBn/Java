4824bae46bbd76fc65067a8edf4bdc73b0409a99
HonorBn <31815325+Youngbin-Ahn@users.noreply.github.com>
2017-10-28 16:33:33 +0900

    Greedy

--- 04. Greedy/Main1183_동전자판기_하.java
+++ 04. Greedy/Main1183_동전자판기_하.java
@@ -3,7 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main1183_µ¿ÀüÀÚÆÇ±â_ÇÏ {
+/* URL
+   http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=466&sca=50&sfl=wr_hit&stx=1183&sop=and
+*/
+
+public class Main1183_ë™ì „ìíŒê¸°_í•˜ {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
