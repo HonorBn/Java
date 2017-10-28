0e97ae7213dcdab624ab969a84e647f2af637631
HonorBn <31815325+Youngbin-Ahn@users.noreply.github.com>
2017-10-28 16:58:38 +0900

    Shortest Path

--- 05. Shortest Path/Main1389_케빈베이컨의6단계법칙.java
+++ 05. Shortest Path/Main1389_케빈베이컨의6단계법칙.java
@@ -10,32 +10,32 @@ import java.util.StringTokenizer;
 */
 
 public class Main1389_케빈베이컨의6단계법칙 {
-	
+
 	static User friends[];
 	static int n, m, kevin[], kevins[];
 	static boolean visit[];
-	
-    public static void main(String[] args) throws Exception {
-    	
-    	input();
-		
+
+	public static void main(String[] args) throws Exception {
+
+		input();
+
 		kevin();
-		
+
 		print();
-    }
-    
-    private static void input() throws IOException {
-		
+	}
+
+	private static void input() throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		
+
 		kevin = new int[n];
 		kevins = new int[n];
 		friends = new User[n];
 		visit = new boolean[n];
-		
+
 		int a, b;
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine().trim());
@@ -45,67 +45,68 @@ public class Main1389_케빈베이컨의6단계법칙 {
 			friends[b] = new User(a, 1, friends[b]);
 		}
 	}
-    
-    private static void kevin() {
-		
-    	PriorityQueue<User> que = new PriorityQueue<>();
-    	
-    	int start, from, fromKevin, to, toKevin;
-    	User fromUser, toUser;
-    	for (int i = 0; i < n; i++) {
-    		
-    		start = i;
-    		que.clear();
-    		Arrays.fill(visit, false);
-    		Arrays.fill(kevin, Integer.MAX_VALUE);
-    		
-    		kevin[start] = 0;
-    		que.offer(new User(start, kevin[start]));
-    		
-    		while (!que.isEmpty()) {
-    			
-    			fromUser = que.poll();
-    			from = fromUser.num;
-    			fromKevin = fromUser.kevin;
-    			
-    			if (visit[from]) continue;
-    			visit[from] = true;
-    			
-    			toUser = friends[from];
-    			while (toUser != null) {
-    				
-    				to = toUser.num;
-    				toKevin = toUser.kevin;
-    				
-    				if (!visit[to] &&
-    					fromKevin + toKevin < kevin[to]) {
-    					
-    					kevin[to] = fromKevin + toKevin;
-    					que.offer(new User(to, kevin[to]));
-    					
-    				}
-    				toUser = toUser.next;
-    			}
-    		}
-    		for (int kevin : kevin) kevins[i] += kevin;
-    	}
-    	
+
+	private static void kevin() {
+
+		PriorityQueue<User> que = new PriorityQueue<>();
+
+		int start, from, fromKevin, to, toKevin;
+		User fromUser, toUser;
+		for (int i = 0; i < n; i++) {
+
+			start = i;
+			que.clear();
+			Arrays.fill(visit, false);
+			Arrays.fill(kevin, Integer.MAX_VALUE);
+
+			kevin[start] = 0;
+			que.offer(new User(start, kevin[start]));
+
+			while (!que.isEmpty()) {
+
+				fromUser = que.poll();
+				from = fromUser.num;
+				fromKevin = fromUser.kevin;
+
+				if (visit[from]) continue;
+				visit[from] = true;
+
+				toUser = friends[from];
+				while (toUser != null) {
+
+					to = toUser.num;
+					toKevin = toUser.kevin;
+
+					if (!visit[to] && fromKevin + toKevin < kevin[to]) {
+
+						kevin[to] = fromKevin + toKevin;
+						que.offer(new User(to, kevin[to]));
+
+					}
+					toUser = toUser.next;
+				}
+			}
+			for (int kevin : kevin)
+				kevins[i] += kevin;
+		}
+
 	}
-    
-    private static void print() {
-    	
-    	int minKevin = Integer.MAX_VALUE;
-    	for (int kevin : kevins)
-    		if (kevin < minKevin) minKevin = kevin;
-    	
-    	for (int i = 0; i < n; i++) {
-    		if (kevins[i] == minKevin) {
-    			System.out.println(i+1);
-    			break;
-    		}
-    	}
-    	
-    }
+
+	private static void print() {
+
+		int minKevin = Integer.MAX_VALUE;
+		for (int kevin : kevins)
+			if (kevin < minKevin)
+				minKevin = kevin;
+
+		for (int i = 0; i < n; i++) {
+			if (kevins[i] == minKevin) {
+				System.out.println(i + 1);
+				break;
+			}
+		}
+
+	}
 }
 
 class User implements Comparable<User> {
@@ -121,6 +122,6 @@ class User implements Comparable<User> {
 		this.next = next;
 	}
 	public int compareTo(User other) {
-		return this.kevin-other.kevin;
+		return this.kevin - other.kevin;
 	}
 }
