91f42140fea34ee4f932248cd4e9cabdcc5f085f
HonorBn <31815325+Youngbin-Ahn@users.noreply.github.com>
2017-10-28 16:34:35 +0900

    Greedy

--- 04. Greedy/Main4307_개미.java
+++ 04. Greedy/Main4307_개미.java
@@ -3,7 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main4307_°³¹Ì_¹éÁØ {
+/* URL
+   https://www.acmicpc.net/problem/4307
+*/
+
+public class Main4307_Â°Â³Â¹ÃŒ_Â¹Ã©ÃÃ˜ {
 	
 	public static void main(String[] args) throws IOException {
 		
